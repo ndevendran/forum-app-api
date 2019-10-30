2ab0e859c47156a43f6e23e61409dceb41568c28
Niroshan Devendran <ndevendran@Niroshans-MacBook-Pro.local>
2019-10-29 22:29:29 -0700

    Changed serverless to allow s3 access

--- upload_avatar.js
+++ upload_avatar.js
@@ -1,15 +1,24 @@
 import AWS from "aws-sdk";
 import { success, failure } from './libs/response-lib.js';
 
-export async function main() {
-  const s3 = new AWS.S3();
-  const storeKey = event.requestContext.user + process.env.avatarFileName;
-  const params = {Bucket: process.env.bucketName, Key: storeKey, Body: data.file };
-  s3.upload(params, (err, data) => {
-    if (error) {
-      return failure({status: false, error: error});
-    }
+export async function main(event, context, callback) {
 
-    return success({storeKey: storeKey });
-  });
+  try {
+    const data = JSON.parse(event.body);
+
+    const s3 = new AWS.S3();
+    const storeKey = event.requestContext.user + process.env.avatarFileName;
+    const params = {Bucket: process.env.bucketName, Key: storeKey, Body: data.file };
+
+    s3.upload(params, (error, data) => {
+      if (error) {
+        return failure({status: false, error: error});
+      }
+
+      return success({storeKey: storeKey });
+    });
+  } catch (e) {
+    return failure({status: false, error: e});
+  }
+
 }
