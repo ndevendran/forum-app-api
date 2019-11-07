8e8bdd911ffd5554d92df14aa355b6463e9da687
Niroshan Devendran <ndevendran@Niroshans-MacBook-Pro.local>
2019-11-06 16:57:27 -0800

    Added verify function. Turned sync promise into multiple async calls

--- update_post.js
+++ update_post.js
@@ -1,8 +1,82 @@
 import * as dynamoDbLib from "./libs/dynamodb-lib.js";
 import { success, failure } from "./libs/response-lib";
 
-export async function main(event, context) {
+const jwt = require('jsonwebtoken');
+const jwkToPem = require('jwk-to-pem');
+const http  = require('https');
+
+async function getKey() {
+  return new Promise(function(resolve, reject) {
+    http.get(process.env.publicKeyURL, (resp) => {
+      let data = '';
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      resp.on('end', () => {
+        data = JSON.parse(data);
+        var pemArray = data["keys"].map((jwkKey) => jwkToPem(jwkKey));
+        resolve(pemArray);
+
+      }).on("error", (err) => {
+        reject(err);
+      });
+    });
+  });
+}
+
+async function verifyToken(event) {
+  const keys = await getKey().then(data => {
+    return data;
+  }).catch(err => {
+    throw err;
+  });
   const data = JSON.parse(event.body);
+  const verified = keys.map((key) => jwt.verify(data.idToken, key, { algorithms: ['RS256']}, function (err, decodedToken) {
+    if(err) {
+      return false;
+    } else {
+      return true;
+    }
+  }));
+  for(var i = 0; i<verified.length; i++) {
+    if(verified[i]) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+async function authorizeEdit(event) {
+  const data = JSON.parse(event.body);
+  var decoded = jwt.decode(data.idToken);
+  const username = decoded['cognito:username'];
+
+  const params = {
+    TableName: process.env.tableNameUserPosts,
+    Key: {
+      postId: event.pathParameters.postId
+    }
+  };
+
+  const result = await dynamoDbLib.call("get", params);
+
+  if(result.Item) {
+    const post = result.Item;
+    if(post.posterUsername !== username) {
+      throw new Error("Access Denied");
+    } else {
+      return {event: event, data: data};
+    }
+  } else {
+    throw new Error("Item not found");
+  }
+}
+
+async function postEdit(event) {
+  const data = JSON.parse(event.body);
+
   const params = {
     TableName: process.env.tableNameUserPosts,
     Key: {
@@ -16,11 +90,29 @@ export async function main(event, context) {
     ReturnValues: "ALL_NEW"
   };
 
-  try {
-    const post = await dynamoDbLib.call("update", params);
-    return success({ status: true, post: post });
-  } catch (e) {
-    console.log(e);
-    return failure({ status: false, error: e });
+  const post = await dynamoDbLib.call("update", params);
+  return success({ status: true, post: post});
+}
+
+function handleErrors(error) {
+  return failure({ status: false, error: error.message });
+}
+
+
+export async function main(event, context) {
+  var authorized = await authorizeEdit(event);
+  var verified   = await verifyToken(event);
+  var response = null;
+
+  if(authorized && verified) {
+    try {
+      response = postEdit(event);
+    } catch (e) {
+      response = handleErrors(e);
+    }
+  } else {
+    response = handleErrors(new Error("Access Denied"));
   }
+
+  return response;
 }
