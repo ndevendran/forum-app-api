96528d8ee39942ec3ff6f37bcf9f050cc029085c
Niroshan Devendran <ndevendran@Niroshans-MacBook-Pro.local>
2019-11-08 00:01:33 -0800

    Refactored some authorization into a library. Finished update post. Prepped update comment but want to deploy update post to dev for testing.

--- update_post.js
+++ update_post.js
@@ -1,52 +1,8 @@
 import * as dynamoDbLib from "./libs/dynamodb-lib.js";
 import { success, failure } from "./libs/response-lib";
+import * as authLib from "./libs/auth-lib.js";
 
 const jwt = require('jsonwebtoken');
-const jwkToPem = require('jwk-to-pem');
-const http  = require('https');
-
-async function getKey() {
-  return new Promise(function(resolve, reject) {
-    http.get(process.env.publicKeyURL, (resp) => {
-      let data = '';
-      resp.on('data', (chunk) => {
-        data += chunk;
-      });
-
-      resp.on('end', () => {
-        data = JSON.parse(data);
-        var pemArray = data["keys"].map((jwkKey) => jwkToPem(jwkKey));
-        resolve(pemArray);
-
-      }).on("error", (err) => {
-        reject(err);
-      });
-    });
-  });
-}
-
-async function verifyToken(event) {
-  const keys = await getKey().then(data => {
-    return data;
-  }).catch(err => {
-    throw err;
-  });
-  const data = JSON.parse(event.body);
-  const verified = keys.map((key) => jwt.verify(data.idToken, key, { algorithms: ['RS256']}, function (err, decodedToken) {
-    if(err) {
-      return false;
-    } else {
-      return true;
-    }
-  }));
-  for(var i = 0; i<verified.length; i++) {
-    if(verified[i]) {
-      return true;
-    }
-  }
-
-  return false;
-}
 
 async function authorizeEdit(event) {
   const data = JSON.parse(event.body);
@@ -101,7 +57,7 @@ function handleErrors(error) {
 
 export async function main(event, context) {
   var authorized = await authorizeEdit(event);
-  var verified   = await verifyToken(event);
+  var verified   = await authLib.verifyToken(event);
   var response = null;
 
   if(authorized && verified) {
