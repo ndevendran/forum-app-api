ec351721ecb81d7033811fe0a0a2042aa0da5f13
Niroshan Devendran <ndevendran@Niroshans-MacBook-Pro.local>
2019-11-12 17:05:06 -0800

    Added like post service

---
+++ like_post.js
@@ -0,0 +1,38 @@
+import * as dynamoDbLib from './libs/dynamodb-lib.js';
+import {success, failure} from './libs/response-lib.js';
+
+
+export async function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+
+  const getParams = {
+    TableName: process.env.tableNameUserLikes,
+    Key: {
+      postId: event.pathParameters.postId,
+      username: data.username
+    }
+  };
+
+  const putParams = {
+    TableName: process.env.tableNameUserLikes,
+    Item: {
+      postId: event.pathParameters.postId,
+      username: data.username,
+      createdAt: Date.now(),
+      like: data.like
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call("get", getParams);
+    if (result.Item) {
+      return failure({status: false, error: "Already liked this post..."});
+    } else {
+      dynamoDbLib.call("put", putParams);
+      return success({status: true});
+    }
+
+  } catch(err) {
+    return failure({status: false, error: err.message});
+  }
+}
